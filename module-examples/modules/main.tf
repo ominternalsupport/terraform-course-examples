4d3a11abe72c8b797c1bf01388c668f45f73c962
ominternalsupport <95745731+ominternalsupport@users.noreply.github.com>
2022-10-10 22:39:09 +0200

    Update main.tf

--- module-examples/modules/main.tf
+++ module-examples/modules/main.tf
@@ -11,11 +11,6 @@ output "instance" {
 	value = aws_instance.webserver
 }
 
-resource "aws_subnet" "webserver" {
-	vpc_id     = var.vpc_id
-	cidr_block = var.cidr_block
-}
-
 resource "aws_instance" "webserver" {
 	ami           = var.ami
 	instance_type = var.instance_type
